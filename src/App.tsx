392934c9e50531da207a4bea45ca73d8fee1891b
Angelo Filho <angelo.filho06@hotmail.com>
2023-01-02 16:12:48 -0300

    fix: remoev unused taskm component import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
-import { Task } from "./components/Task";
 import { TaskForm } from "./components/TaskForm";
 
 import { v4 as uuidV4 } from "uuid";
